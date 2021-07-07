53cdf816e0d368bc29bc9ffa7dd2c527a62015c6
sangung <tkddnd010@naver.com>
2021-07-07 13:59:53 +0900

    Link 태그 적용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
 import MainPageComponent from "./main/main.js";
+import { Switch, Route } from "react-router-dom";
+import Uploadpage from "./upload";
+import Productpage from "./product";
+
 function App() {
-  return <MainPageComponent />;
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path="/">
+          <MainPageComponent />
+        </Route>
+        <Route exact={true} path="/products/:id">
+          <Productpage />
+        </Route>
+        <Route exact={true} path="/upload">
+          <Uploadpage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
